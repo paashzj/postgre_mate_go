706859df4d17814ef4c114f8cdfcfdece0864201
lovehzj <1922919664@qq.com>
2022-01-14 07:25:43 +0800

    Release docker image binary

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -2,6 +2,6 @@
 
 export REMOTE_MODE=false
 
-mkdir $POSTGRE_HOME/logs
+mkdir -p $POSTGRE_HOME/logs
 nohup $POSTGRE_HOME/mate/postgre_mate >>$POSTGRE_HOME/logs/postgre_mate.stdout.log 2>>$POSTGRE_HOME/logs/postgre_mate.stderr.log
 
